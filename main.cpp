8e4120c37d6dfc753d941adf5e26446aa648352d
Judith Maigler <judith.maigler@bmw.de>
2024-11-21 13:16:14 +0100

    main and clang-tidy

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+auto main() -> int
+{
+	const int maximum = 10;
+	for(int i=0; i<maximum; i++) {
+		cout << "Es Weihnachtet sehr" << endl;
+	}
+
+	return 0;
+}
